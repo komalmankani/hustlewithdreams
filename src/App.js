49d74928a5f6516bdba66b9ce0301c5a4643afc1
k4komalmankani <komalmankani0104@gmail.com>
2024-02-23 22:28:03 +0530

    first

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3 class="">Hustle With Dreams</h3>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Opening Soon.....
         </a>
       </header>
     </div>
